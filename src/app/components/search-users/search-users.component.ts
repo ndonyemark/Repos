3b2d31398f51e52e7cc2939c7a56da965fbe9a67
ndonye mark <ndonyemark@gmail.com>
2020-04-13 23:09:58 +0300

    imported http client and search user interface file

--- src/app/components/search-users/search-users.component.ts
+++ src/app/components/search-users/search-users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { searchUser } from './search-users.interface';
 
 @Component({
   selector: 'app-search-users',
