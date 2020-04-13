2b7ec03de838de1b7cb364f08d589943e1ed3dc3
ndonye mark <ndonyemark@gmail.com>
2020-04-13 23:08:19 +0300

    added search-users interface class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { SearchUsersComponent } from './components/search-users/search-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    SearchUsersComponent
   ],
   imports: [
     BrowserModule,
