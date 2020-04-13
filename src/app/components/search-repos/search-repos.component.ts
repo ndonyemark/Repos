a4cc86134312250a840a06bc9c4535e374fe966b
ndonye mark <ndonyemark@gmail.com>
2020-04-13 23:20:50 +0300

    get data through search repos

--- src/app/components/search-repos/search-repos.component.ts
+++ src/app/components/search-repos/search-repos.component.ts
@@ -16,7 +16,16 @@ export class SearchReposComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   searchRepos(){
-    
+    setTimeout(() => {
+      this.userPrompt = "Be patient while we get you the repositories..."
+    }, 200);
+    this.http.get<userRepo>('https://api.github.com/search/repositories?q=' + this.keyword + '').toPromise().then((data) => {
+      console.log(data);
+      this.userRepo = data;
+    })
+    setTimeout(()=>{
+      this.userPrompt = '';
+    }, 3000);
   }
 
   ngOnInit(): void {
