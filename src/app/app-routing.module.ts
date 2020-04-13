9b0dbf310c701b4a9321f41a41562f2a51b88bfb
ndonye mark <ndonyemark@gmail.com>
2020-04-13 23:24:39 +0300

    imported the components for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SearchReposComponent } from './components/search-repos/search-repos.component';
+import { SearchUsersComponent } from './components/search-users/search-users.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
 
 const routes: Routes = [];
 
